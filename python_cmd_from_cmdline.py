e800a956ec3ea6b4a2cc97bf87fcdef686bf5f74
Neelaksh <nilaxch1@gmail.com>
2018-01-14 21:06:20 +0530

    Removed usless functions, cleaned up a bit

--- python_cmd_from_cmdline.py
+++ python_cmd_from_cmdline.py
@@ -1,34 +1,30 @@
 import subprocess
 import os
-import requests
 import sys
 
 def run_cmd(cmd):
     split_cmd = cmd.strip().split(" ")
     subprocess.call(split_cmd)
 
-def run_for_all_subdirs(directory_path,cmd):
+def run_for_all_subdirs(directory_path,list_of_cmds):
     list_of_subdirs = os.listdir(directory_path)
-    for file in list_of_subdirs:
-        os.chdir(file)
+    for directory in list_of_subdirs:
+        os.chdir(directory)
         print(os.getcwd())
-        run_cmd(cmd)
+        run_chain_of_commands_one_dir(directory,list_of_cmds)
         os.chdir("../")
 
 
 #HOW TO USE: first parameter is the path opf the folder you want to go to
 #, after that just type in the command you want to run
 # e.g python script.py **path** git pull origin master , mvn clean compile
-def run_chain_of_cmds(dir, list_of_cmds):
-    for cmd in list_of_cmds:
-        run_for_all_subdirs(dir,cmd)
 
 
 def run_chain_of_commands_one_dir(dir, list_of_cmds):
     for cmd in list_of_cmds:
         run_cmd(cmd)
 
-def args_to_list_of_commands(*args):
+def args_to_list_of_commands(args):
     cmds = " ".join(sys.argv[2:])
     list_of_cmds = cmds.split(",")
     print(list_of_cmds)
@@ -42,6 +38,6 @@ if __name__ =='__main__':
         list_of_cmds = args_to_list_of_commands(sys.argv)
         print(list_of_cmds)
         curr_dir = os.getcwd()
-        run_chain_of_cmds(curr_dir,list_of_cmds)
+        run_for_all_subdirs(curr_dir,list_of_cmds)
 
 
